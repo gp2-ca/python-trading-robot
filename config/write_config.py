8b4f78d7e892ff521f65d0df5d2d96a82266d267
Patrick Payeur <payeurp@hotmail.com>
2022-03-25 14:25:07 -0400

    Added config parameters for alpaca account

--- config/write_config.py
+++ config/write_config.py
@@ -8,6 +8,10 @@ config.set('main', 'CLIENT_ID', 'FAKE_CLIENT_ID')
 config.set('main', 'REDIRECT_URI', 'FAKE_REDIRECT_URL')
 config.set('main', 'JSON_PATH', 'FAKE_PATH')
 config.set('main', 'ACCOUNT_NUMBER', 'FAKE_ACCT')
+config.set('main', 'API_KEY', 'FAKE_KEY')
+config.set('main', 'SECRET_KEY', 'FAKE_SECRET_KEY')
+config.set('main', 'BASE_URL', 'FAKE_URL')
+config.set('main', 'DATA_URL', 'FAKE_URL')
 
 with open(file='config/config.ini', mode='w') as f:
     config.write(f)
